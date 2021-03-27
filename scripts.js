6aaaea055b5d5963f0510b5cf718a43a058a0cd2
efoejean <efoejean@gmail.com>
2021-03-27 17:00:47 -0500

    Prevent default behaviour after submitting

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ document.querySelector("#username").addEventListener("blur", () => {
   console.log("Left input");
 });
 
-document.querySelector("form").addEventListener("submit", () => {
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
   console.log("Submitted form!");
 });
