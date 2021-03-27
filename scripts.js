e79cb09b3e564f4887d866caec1b9bd3e3da4673
efoejean <efoejean@gmail.com>
2021-03-27 16:43:53 -0500

    Log "Left input" when there is blur on username

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 document.querySelector("#username").addEventListener("focus", () => {
   console.log("Enter input!");
 });
+
+document.querySelector("#username").addEventListener("blur", () => {
+  console.log("Left input");
+});
