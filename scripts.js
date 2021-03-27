0115e40cbc028fccc4b0578305f8da31d3f194cc
efoejean <efoejean@gmail.com>
2021-03-27 16:50:25 -0500

    log "Submitted form" when click the submit buttom

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-console */
 document.querySelector("#username").addEventListener("focus", () => {
   console.log("Enter input!");
 });
 
 document.querySelector("#username").addEventListener("blur", () => {
   console.log("Left input");
+});
+
+document.querySelector("form").addEventListener("submit", () => {
+  console.log("Submitted form!");
 });
